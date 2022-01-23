a08cfb41948427a08067fedd05bddbe4bf1e4cde
Alexo <74928088+Alekso91@users.noreply.github.com>
2022-01-23 20:16:49 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,161 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS] });
+const threads = [];
+const users = [];
+require('dotenv').config()
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+function hasRole(member, id) {
+    if(member.roles.cache.find(r => r.id == id) != null) {
+        return true;
+    }
+    return false;
+}
+
+function getThread(guild, id) {
+    var thread = guild.channels.cache.get(id);
+    if(thread && thread.type == "GUILD_PUBLIC_THREAD") {
+        return thread
+    };
+    return null
+}
+
+client.on('messageCreate', async (message) => {
+    if(message.content.startsWith(process.env.PREFIX+process.env.COMMAND)) {
+        if(hasRole(message.member, process.env.ADMIN_ROLE)) {
+            var args = message.content.toLowerCase().replace(process.env.PREFIX+process.env.COMMAND+" ", "").split(" ")
+            if(args[0] == "add") {
+                ids = args[1].replace("```", "").replace("```", "").split("\n");
+                total = 0;
+                valid = 0;
+                //getting threads
+                ids.forEach(e => {
+                    if(e != "") {
+                        total ++;
+                        var t = getThread(message.guild, e)
+                        if(t) {
+                            valid ++;
+                            threads.push(e)
+                        }
+                    }
+                });
+                message.channel.send(valid+"/"+total+" threads were added");
+            } else if(args[0] == "remove") {
+                ids = args[1].replace("```", "").replace("```", "").split("\n");
+                total = 0;
+                valid = 0;
+                //getting threads
+                ids.forEach(e => {
+                    if(e != "") {
+                        total ++;
+                        var t = getThread(message.guild, e)
+                        if(t) {
+                            valid ++;
+                            index = threads.indexOf(e);
+                            if(index > -1) {
+                                threads.splice(index, 1)
+                            }
+                        }
+                    }
+                });
+                message.channel.send(valid+"/"+total+" threads were removed");
+            } else if(args[0] == "list") {
+                var constructor = "```{text}\n```"
+                var text = ""
+                if(threads.length == 0) {
+                    text = "None"
+                } else {
+                    threads.forEach(id => {
+                        text = `${text}\n${id}`
+                    })
+                }
+                message.channel.send(constructor.replace("{text}", text))
+            } else if(args[0] == "top") {
+                msg2 = message;
+                //args[1] = % requirement | args[2] = word requirement
+                if(args[1] && args[2]) {
+                    if(!isNaN(args[1]) && !isNaN(args[2])) {
+                        let messages = [];
+                        let status = await message.channel.send(":blue_circle: Collecting Data... :blue_circle:")
+                        for(const id of threads) {
+                            let channel = client.channels.cache.get(id);
+                            let msgs = [];
+                            let last_id;
+                            while(true) {
+                                var options = { limit: 100 };
+                                if (last_id) {
+                                    options.before = last_id;
+                                };
+                                a = await channel.messages.fetch(options);
+                                for(const b of a) {
+                                    msgs.push(b);
+                                };
+                                last_id = msgs[msgs.length-1][0];
+                                if(a.size != 100) {
+                                    break;
+                                }
+                            };
+                            messages.push(msgs);
+                        };
+                        if(messages) {
+                            let map = new Map();
+                            let total = 0;
+                            for(var i of messages) {
+                                for(b of i) {
+                                    total++;
+                                    id = b[1].author.id;
+                                    message = b[1].content.split(" ");
+                                    data = map.get(id);
+                                    if(message.length >= args[2]) {
+                                        if(data) {
+                                            data++;
+                                            map.delete(id);
+                                            map.set(id, data);
+                                        } else {
+                                            map.set(id, 1);
+                                        };
+                                    }
+                                };
+                            };
+                            if(map != null) {
+                                status.edit(":green_circle: Processing Data :green_circle:")
+                                let passing = [];
+                                for (var i of map) {
+                                    if((i[1]/total)*100 >= args[1]) {
+                                        passing.push(i[0]);
+                                    };
+                                };
+                                for (var i of passing) {
+                                    try {
+                                        guild = msg2.guild;
+                                        member = guild.members.cache.get(i);
+                                        role = guild.roles.cache.find(role => role.id === process.env.REWARD_ROLE);
+                                        member.roles.add(role);
+                                        status.edit(":green_circle: Data Processed Successfully :green_circle:")
+                                    } catch(e) {
+                                        console.log(e)
+                                    };
+                                }
+                            }
+                        } else {
+                            status.edit(":red_circle: Gathering Data Failed :red_circle:")
+                        }
+                    } else {
+                        message.channel.send(":x: Invalid Argument")
+                    }
+                } else {
+                    message.channel.send(":x: Missing Arguments")
+                }
+            } else {
+                message.channel.send(":x: Invalid Argument")
+            }
+        } else {
+            message.channel.send(":x: You cannot do this")
+        }
+    }
+});
+  
+client.login(process.env.TOKEN);
